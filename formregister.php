cdf1184b9b0bfd97c5ff502fa8a02bf6b106f915
Albertus J.W <72346370+albertus-jw@users.noreply.github.com>
2020-10-04 22:59:28 +0700

    Revert "Add files via upload"

--- formregister.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo"<h2>Silakan Register</h2>";
-echo "<form method=post action=registration.php><table border=0>";
-echo "<tr><td>Username: </td><td>: <input type=text name=uname> </td></tr>";
-echo "<tr><td>Password </td><td>: <input type=password name=pass> </td></tr>";
-echo "<tr><td>Nama </td><td>: <input type=text name=newname> </td></tr>";
-echo "<tr><td><input type=submit value=Input name=add></td></tr>"; //tombol login
-echo "</table></form>";
-?>
